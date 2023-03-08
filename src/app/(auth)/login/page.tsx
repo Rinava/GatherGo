6f9299deaa481f26c66082e0a903a36014a5a633
Lara Mateo <asdasd5056@gmail.com>
2023-03-08 14:43:06 -0300

    Auth init

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+import LoginBtn from './../../../../components/loginBtn';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login to your account',
+};
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Link href='/'>
+        <>Back</>
+      </Link>
+      <div>
+        <div>
+          <h1>Welcome back</h1>
+          <p>Enter your email to sign in to your account</p>
+          <LoginBtn />
+        </div>
+        <p>
+          <Link href='/register'>Don&apos;t have an account? Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
